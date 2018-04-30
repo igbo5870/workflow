3e40f9205da8741d1b8e11d98c56934bb4db4488
Grégory <gregory.bouloc@gmail.com>
2018-04-30 14:15:55 +0200

    sass finish

--- 02_gulp/gulpfile.js
+++ 02_gulp/gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+//var plugins = require('gulp-load-plugins')();
+var sass = require('gulp-sass'); 
+var source = './sources'; // dossier de travail
+var destination = './build'; // dossier à livrer
 
-gulp.task('my-task', function() {
-  // place code for your my-task task here
+// Compilation de SASS vers CSS
+//******************************************
+gulp.task('sass', function () {
+  return gulp.src('static/sources/scss/app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('static/build/css/'));
 });
